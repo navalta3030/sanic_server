4d98a75d2d28f5f2346727a6e2fa1cde09f5b15f
Mark Christian Navalta <navalta3030@gmail.com>
2020-04-04 02:55:04 -0700

    random

--- app.py
+++ app.py
@@ -3,22 +3,23 @@ import os
 from sanic import Sanic
 from sanic import response
 from utils import utils, logging
+from sanic_cors import CORS
 
 logger = logging.__logger__()
 
 # Set logger to override default basicConfig
 sanic = Sanic()
+CORS(sanic)
 
 
 @sanic.route("/")
 def test(request):
-    logger.info("recedasda'hey'")
-    text = "asdasd"
-    return response.text(text)
+    return response.json({'message': 'Hello world!'})
 
 
 if utils.isDevelopment():
     logger.info("Starting localhost development")
+
     sanic.run(host="0.0.0.0", port=8000, debug=True)
 else:
     logger.info("Starting production server")
